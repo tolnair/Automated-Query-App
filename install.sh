228de7008b4c7e532991147bd6838e77e28ddb04
TolnaiRichard <richardtolnai@gmail.com>
2021-03-03 17:18:54 +0100

    Run this file

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo apt-get install -y docker-compose
 sudo docker-compose up -d db web
+echo "Now you can use the Query App at localhost:8080"
